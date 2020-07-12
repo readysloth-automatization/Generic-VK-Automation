d4f889a4d518e5e39ab3b7e459dfd9f78b0f492a
readysloth <readysloth@protonmail.com>
2020-07-12 22:56:12 +0300

    Добавил получение частичной функции постинга

--- vk_poster.py
+++ vk_poster.py
@@ -1,8 +1,13 @@
 import sys
+import time
+import typing
 import asyncio
+import datetime as d
+import functools as f
 
 import vkwave.api as vapi
 import vkwave.api.token.token as t
+import vkwave.client as client
 import vkwave.client.abstract as abstr
 
 def make_token(token: str, token_type: t.AnyABCToken) -> t.AnyABCToken:
@@ -18,3 +23,29 @@ def make_api_context(token: str,
                      token_type: t.AnyABCToken,
                      client_type: abstr.AbstractAPIClient) -> vapi.APIOptionsRequestContext:
     return make_api(token, token_type, client_type).get_context()
+
+def get_wall_post_func(id: int,
+                       message: str,
+                       api_context: vapi.APIOptionsRequestContext,
+                       user: bool = False,
+                       attachments: list = None,
+                       date: d.datetime = None) -> typing.Callable:
+
+    wall_post_method = f.partial(api_context.wall.post,
+                                 message=message,
+                                 attachments=attachments,
+                                 publish_date=int(date.timestamp()) if date else None
+                                 )
+
+    if user:
+        return f.partial(wall_post_method, owner_id=id)
+    else:
+        return f.partial(wall_post_method, owner_id=-id)
+
+
+async def test():
+    concrete_api = make_api_context(sys.argv[1], t.UserSyncSingleToken, client.AIOHTTPClient)
+    
+    return concrete_api
+
+print(asyncio.get_event_loop().run_until_complete(test()))
